120cd1ded8097c66011c60dca866306e85db56d9
q42355050 <inanxlvi@hotmail.com>
2020-12-14 08:56:59 +0000

    ,

--- lab8/example1.py
+++ lab8/example1.py
@@ -1,4 +1,4 @@
-def sum_of_list(a_list):
+def sum_of_list_square(a_list):
   total = 0
   for i in a_list:
     total += i
@@ -7,4 +7,4 @@ def sum_of_list(a_list):
   return result
 
 a_list = [12, -7, 5, -89.4, 3, 27, 56, 57.3]
-print(sum_of_list(a_list))
+print(sum_of_list_square(a_list))
